a1bf30bf1aada5eb2a73557ba9b495b2c8f99950
Retro <gitmax40a@mail.ru>
2016-05-17 20:15:53 +0300

    add new entity: user, grade, message, forumCategory

---
+++ src/main/java/sekta/platform/core/entity/User.java
@@ -0,0 +1,86 @@
+package sekta.platform.core.entity;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Retro on 14.05.2016.
+ */
+
+@Entity
+@Table(name="users_table")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="user_name")
+    private String userName;
+
+    @Column(name="password")
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Grade> gradeSet= new HashSet<>();
+
+    @OneToMany(mappedBy = "user")
+    private Set<Message> messageSet= new HashSet<>();
+
+
+    public User() {}
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Set<Grade> getGradeSet() {
+        return gradeSet;
+    }
+
+    public void setGradeSet(Set<Grade> gradeSet) {
+        this.gradeSet = gradeSet;
+    }
+
+    public Set<Message> getMessageSet() {
+        return messageSet;
+    }
+
+    public void setMessageSet(Set<Message> messageSet) {
+        this.messageSet = messageSet;
+    }
+}
