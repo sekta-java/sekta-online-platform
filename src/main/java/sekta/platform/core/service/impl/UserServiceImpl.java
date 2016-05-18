4ee0baf137fd9b43f7514498630f591786fbdfc8
NataliyaD-in6k <NataliyaD@softwareplanet.uk.com>
2016-05-18 16:36:52 +0300

    Add anotations to:  - UserServiceImpl  - UserDaoImpl

--- src/main/java/sekta/platform/core/service/impl/UserServiceImpl.java
+++ src/main/java/sekta/platform/core/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package sekta.platform.core.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sekta.platform.core.dao.UserDao;
 import sekta.platform.core.entity.User;
 import sekta.platform.core.service.UserService;
@@ -10,33 +12,39 @@ import java.util.List;
 /**
  * Created by natad on 18.05.2016.
  */
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     UserDao userDao;
 
     @Override
+    @Transactional
     public List<User> getAllUsers() {
         return userDao.findAll();
     }
 
     @Override
+    @Transactional
     public User getUserById(Long id) {
         return userDao.getById(id);
     }
 
     @Override
+    @Transactional
     public User createUser(User user) {
         userDao.create(user);
         return user;
     }
 
     @Override
+    @Transactional
     public User updateUser(User user) {
         userDao.update(user);
         return user;
     }
 
     @Override
+    @Transactional
     public void deleteUser(Long id) {
         User user = new User();
         user.setId(id);
--- src/main/java/sekta/platform/persistence/UserDaoImpl.java
+++ src/main/java/sekta/platform/persistence/UserDaoImpl.java
@@ -1,11 +1,13 @@
 package sekta.platform.persistence;
 
+import org.springframework.stereotype.Repository;
 import sekta.platform.core.dao.UserDao;
 import sekta.platform.core.entity.User;
 
 /**
  * Created by natad on 18.05.2016.
  */
+@Repository
 public class UserDaoImpl extends BaseDaoImpl<User> implements UserDao{
     @Override
     public User getById(Long id) {
