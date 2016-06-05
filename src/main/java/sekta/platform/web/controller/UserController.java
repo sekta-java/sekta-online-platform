84d34accaa11b5a913a09476a28b2937b5a2293c
FreeFly19 <wmsystem2008@yandex.ru>
2016-06-05 21:26:02 +0300

    Replace create new entity in controller by ModelAttribute annotation

--- src/main/java/sekta/platform/web/controller/UserController.java
+++ src/main/java/sekta/platform/web/controller/UserController.java
@@ -3,10 +3,7 @@ package sekta.platform.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import sekta.platform.core.entity.User;
 import sekta.platform.core.service.UserService;
@@ -27,14 +24,8 @@ public class UserController {
     }
 
     @RequestMapping(value = "create", method = RequestMethod.POST)
-    public String createUser(@RequestParam("userName") String userName,
-                             @RequestParam("email") String email,
-                             @RequestParam("password") String password,
+    public String createUser(@ModelAttribute(value = "user") User user,
                              RedirectAttributes redirectAttributes){
-        User user = new User();
-        user.setUserName(userName);
-        user.setEmail(email);
-        user.setPassword(password);
         userService.createUser(user);
         redirectAttributes.addFlashAttribute("message", "User successfully created!");
         return "redirect:/users";
